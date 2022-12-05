11c8fb21b059370ff57fac54fe307660c7cc6729
Carlos Toledo Silva <72420685+CTS-crypto@users.noreply.github.com>
2022-12-05 02:15:53 -0500

    funciones, arreglos   y presentacion

--- logimg/algorithms/edge_detection.py
+++ logimg/algorithms/edge_detection.py
@@ -6,3 +6,11 @@ def edge_detection(image:np.ndarray,filter):
 def space_edge_detection(image:np.ndarray,filter,space):
     aux_image=space.function(space.gray_tone(image))
     return space.inverse_gray_tone(space.inverse_function(filter(aux_image)))
+
+def parameterized_space_edge_detection(image:np.ndarray,filter,space,*,lambd=None,mu=None):
+    aux_image=space.gray_tone(image)
+    edge_image=space.inverse_function(filter(space.function(aux_image,lambd)),lambd)
+    if mu is not None:
+        return space.inverse_gray_tone(edge_image,mu)
+    else:
+        return space.inverse_gray_tone(edge_image)
--- logimg/algorithms/unsharp_masking.py
+++ logimg/algorithms/unsharp_masking.py
@@ -16,3 +16,18 @@ def space_unsharp_masking(image:np.ndarray,filter,op:str,space):
         return space.inverse_gray_tone(space.inverse_function(aux_image-filter(aux_image)))
     else:
         raise ValueError('Invalid operator')
+
+def parameterized_space_unsharp_masking(image:np.ndarray,filter,op:str,space,*,gamma=None,lambd=None,mu=None):
+    aux_image=space.gray_tone(image)
+    edge_image=space.inverse_function(filter(space.function(aux_image,lambd)),lambd)
+    if op=='+':
+        pre_image=space.sum(aux_image,edge_image,gamma)
+    elif op=='-':
+        pre_image=space.sub(aux_image,edge_image,gamma)
+    else:
+        raise ValueError('Invalid operator')
+    if mu is not None:
+        return space.inverse_gray_tone(pre_image,mu)
+    else:
+        return space.inverse_gray_tone(pre_image)
+    
